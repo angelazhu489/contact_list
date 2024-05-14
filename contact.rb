cce5b837b21498c601a7357a2108320ba45c2c06
angelazhu489 <angelazhu489@gmail.com>
2024-05-13 18:50:18 -0700

    edit contact

--- contact.rb
+++ contact.rb
@@ -9,6 +9,45 @@ configure do
 	also_reload "database_persistence.rb"
 end
 
+helpers do
+	def format_sym_to_label(sym)
+		sym.to_s.gsub("_", " ").capitalize
+	end
+
+	def field_type(field)
+		type = field.to_s
+		case type
+		when "phone_number"
+			"tel"
+		when "email"
+			"email"
+		else
+			"text"
+		end
+	end
+end
+
+# Returns string error for any input of fields
+def error_for_params(fields)
+	first_name, last_name, phone, email = fields.values
+	if first_name.empty? || last_name.empty?
+		"A first and last name is required."
+	elsif phone.length != 10 && !phone.empty?
+		"A phone number must be 10 digits."
+	elsif !email.match(/\A[a-z0-9+_.-]+@[a-z0-9.-]+\Z/i) && !email.empty?
+		"Please enter a valid email"
+	end
+end
+
+# Returns hash of fields with fields that have been unchanged set to nil
+def updated_fields(contact, fields)
+	updated = {}
+	fields.each do |key, val|
+		val != contact[key] ? updated[key] = val : updated[key] = nil
+	end
+	updated
+end
+
 before do
 	@storage = DatabasePersistence.new
 end
@@ -25,18 +64,41 @@ end
 
 # View a single contact
 get "/contacts/:id" do
-	id = params[:id].to_i
-	@contact = @storage.find_contact(id)
+	@id = params[:id].to_i
+	@contact = @storage.find_contact(@id)
 	erb :contact
+end
+
+# Render edit contact form
+get "/contacts/:id/edit" do
+	@id = params[:id].to_i
+	@contact = @storage.find_contact(@id)
+	erb :edit_contact
+end
+
+# Edit a contact
+post "/contacts/:id" do
+	@id = params[:id].to_i
+	fields = { first_name: params[:first_name],
+						 last_name: params[:last_name],
+						 phone_number: params[:phone_number],
+						 email: params[:email] }
+	@contact = @storage.find_contact(@id)
+	error = error_for_params(fields)
+	if error
+		session[:message] = error
+		erb :edit_contact, layout: :layout
+	else
+		updated = updated_fields(@contact, fields)
+		@storage.update_contact(@id, updated) unless updated.values.all?(nil)
+		session[:message] = "The contact has been updated."
+		redirect "/contacts/#{@id}"
+	end
 end
 
 # Render add contact form
 
 # Add a contact
-
-# Render edit contact form
-
-# Edit a contact
 
 # Delete a contact
 
--- database_persistence.rb
+++ database_persistence.rb
@@ -14,12 +14,14 @@ class DatabasePersistence
 		end
 	end
 
+	# Return row from contacts table
 	def find_contact(id)
 		sql = "SELECT * FROM contacts WHERE id = $1"
 		result = @db.exec_params(sql, [id]).first
 		format_tuple(result)
 	end
 
+	# Format sql tuple to application hash
 	def format_tuple(tuple)
 		{ id: tuple["id"].to_i,
 			first_name: tuple["first_name"],
@@ -27,4 +29,34 @@ class DatabasePersistence
 			phone_number: tuple["phone_number"],
 			email: tuple["email"] }
 	end
+
+	# Update row of table 
+	def update_contact(id, fields)
+		method_args = create_update_fields_sql(id, fields)
+		sql = method_args[0]
+		sql_args = method_args[1]
+		@db.exec_params(sql, sql_args)
+	end
+
+	private
+
+	# Return sql statement and arguments to exec_params method
+	def create_update_fields_sql(id, fields)
+		set_conditions = []
+		sql_args = []
+		ind = 1
+		fields.each do |key, val|
+			if val == nil
+				next
+			elsif val == ""
+				set_conditions << "#{key.to_s} = NULL"
+			else
+				sql_args << val
+				set_conditions << "#{key.to_s} = $#{ind}"
+				ind += 1
+			end
+		end
+		set_conditions = set_conditions.join(", ")
+		["UPDATE contacts SET #{set_conditions} WHERE id = #{id}", sql_args]
+	end 
 end
